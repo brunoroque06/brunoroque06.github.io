44dffab4426979693a73c19de59d2561d84faa31
BrunoRoque06 <brunoroque06@gmail.com>
2017-08-23 01:04:52 +0100

    Information added to code.js, and display method created

--- js/code.js
+++ js/code.js
@@ -1,29 +1,63 @@
-
-var goodPractices = [
+var intel = [
     {
-        name: "Block Chain",
-        description: "",
-        link: "",
-        status: "Complete"
+        title: "Comments",
+        entries: [
+        {
+            subtitle: "Inappropriate Information",
+            description: "Alguma informação",
+            example: ""
+        },
+        {
+            subtitle: "Obsolete Comment",
+            description: "Alguma informação",
+            example: ""
+        },
+        {
+            subtitle: "Redundant Comment",
+            description: "Alguma informação",
+            example: ""
+        }]
     },
     {
-        name: "Chess Engine",
-        description: "",
-        link: "",
-        status: "In Progress"
+        title: "Uml",
+        entries: [
+        {
+            subtitle: "Inappropriate Information",
+            description: "Alguma informação",
+            example: ""
+        },
+        {
+            subtitle: "Obsolete Comment",
+            description: "Alguma informação",
+            example: ""
+        },
+        {
+            subtitle: "Redundant Comment",
+            description: "Alguma informação",
+            example: ""
+        }]
     },
-    {
-        name: "Genetic Algorithm",
-        description: "",
-        link: "",
-        status: "Complete"
-    }
 ];
 
-projects.display = function() {
-    projects.forEach(function(project) {
-        document.querySelector("main").append(project.name);
+var stringToReplace = "%data%";
+var htmlHeader3 = '<div class="row"><h3>' + stringToReplace + '</h3></div>';
+var htmlHeader4 = '<div class="row"><h4>' + stringToReplace + '</h4></div>';
+var htmlParagraph = '<div class="row"><p>' + stringToReplace + '</p></div>';
+
+intel.display = function() {
+    var main = $('main');
+    intel.forEach(function(topic) {
+        var header3 = htmlHeader3.replace(stringToReplace, topic.title);
+        main.append(header3);
+
+        topic.entries.forEach(function(entry) {
+            var header4 = htmlHeader4.replace(stringToReplace, entry.subtitle);
+            var paragraph = htmlParagraph.replace(stringToReplace, entry.description);
+
+            main.append(header4);
+            main.append(paragraph);
+        });
     });
 };
 
-// projects.display();
+intel.display();
