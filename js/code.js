bebc8f18cbc87fe40a40be041c7310466dae27d8
BrunoRoque06 <brunoroque06@gmail.com>
2017-08-23 01:28:52 +0100

    References added

--- js/code.js
+++ js/code.js
@@ -19,7 +19,7 @@ var intel = [
         }]
     },
     {
-        title: "Uml",
+        title: "Unified Modeling Language (UML)",
         entries: [
         {
             subtitle: "Inappropriate Information",
@@ -39,6 +39,14 @@ var intel = [
     },
 ];
 
+var references = [
+    "Refactor, by Martin Fowler",
+    "Clean Code, by Robert Martin",
+    "The Little Mocker, https://8thlight.com/blog/uncle-bob/2014/05/14/TheLittleMocker.html",
+    "Clean Code Javascript, https://github.com/ryanmcdermott/clean-code-javascript",
+    "Unified Modeling Language (UML), https://yuml.me/"
+];
+
 var stringToReplace = "%data%";
 var htmlHeader3 = '<div class="row"><h3>' + stringToReplace + '</h3></div>';
 var htmlHeader4 = '<div class="row"><h4>' + stringToReplace + '</h4></div>';
@@ -46,18 +54,30 @@ var htmlParagraph = '<div class="row"><p>' + stringToReplace + '</p></div>';
 
 intel.display = function() {
     var main = $('main');
-    intel.forEach(function(topic) {
-        var header3 = htmlHeader3.replace(stringToReplace, topic.title);
-        main.append(header3);
+    this.forEach(function(topic) {
+        var topicTitle = htmlHeader3.replace(stringToReplace, topic.title);
+        main.append(topicTitle);
 
         topic.entries.forEach(function(entry) {
-            var header4 = htmlHeader4.replace(stringToReplace, entry.subtitle);
-            var paragraph = htmlParagraph.replace(stringToReplace, entry.description);
+            var entryTitle = htmlHeader4.replace(stringToReplace, entry.subtitle);
+            var entryDescription = htmlParagraph.replace(stringToReplace, entry.description);
 
-            main.append(header4);
-            main.append(paragraph);
+            main.append(entryTitle);
+            main.append(entryDescription);
         });
     });
 };
 
+references.display = function() {
+    var main = $('main');
+    var referencesTitle = htmlHeader3.replace(stringToReplace, "References");
+    main.append(referencesTitle);
+
+    this.forEach(function(referenceString) {
+        var reference = htmlParagraph.replace(stringToReplace, referenceString);
+        main.append(reference);
+    });
+};
+
 intel.display();
+references.display();
