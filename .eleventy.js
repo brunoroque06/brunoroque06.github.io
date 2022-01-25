41d16e49461e8459b7401a011ec72f6c266ed88f
Bruno Roque <brunoroque06@gmail.com>
2022-01-25 20:49:18 +0100

    Move build system logic to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,30 @@
+let { shouldBuildCv, buildCv } = require("./util.js");
+let sass = require("sass");
+
 module.exports = function (config) {
+  config.on("eleventy.before", async () => {
+    await buildCv();
+  });
+
+  config.on("eleventy.beforeWatch", async (files) => {
+    if (shouldBuildCv(files)) {
+      await buildCv();
+    }
+  });
+
+  config.addFilter("scss2css", function (content) {
+    return sass.compileString(content, {
+      sourceMap: false,
+      style: "compressed",
+    }).css;
+  });
+
   config.addPassthroughCopy("documents");
   config.addPassthroughCopy("images");
   config.addPassthroughCopy("manifest.json");
   config.addPassthroughCopy("service-worker.js");
+
+  config.addPassthroughCopy({ dist: "documents" });
 
   return {};
 };
--- util.js
+++ util.js
@@ -1,15 +1,29 @@
-let puppeteer = require("puppeteer");
 let fs = require("fs");
+let puppeteer = require("puppeteer");
 
-(async () => {
+function shouldBuildCv(files) {
+  return files.includes("documents/bruno-roque-cv.html");
+}
+
+async function buildCv() {
   let browser = await puppeteer.launch({ headless: true });
   let page = await browser.newPage();
   let html = fs.readFileSync("./documents/bruno-roque-cv.html", "utf8");
   await page.setContent(html, { waitUntil: "domcontentloaded" });
+
+  if (!fs.existsSync("./dist")) {
+    fs.mkdirSync("./dist");
+  }
+
   await page.pdf({
     format: "A4",
-    path: "./documents/bruno-roque-cv.pdf",
+    path: "./dist/bruno-roque-cv.pdf",
     printBackground: true,
   });
   await browser.close();
-})();
+}
+
+module.exports = {
+  shouldBuildCv,
+  buildCv,
+};
