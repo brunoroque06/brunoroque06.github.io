c05bdeac193d8d06207449eea51d56f1e45a5ce6
Bruno Roque <brunoroque06@gmail.com>
2025-05-30 18:49:29 +0200

    Upgrade Angular to 20

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { provideZonelessChangeDetection } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideExperimentalZonelessChangeDetection()],
+  providers: [provideZonelessChangeDetection()],
 });
