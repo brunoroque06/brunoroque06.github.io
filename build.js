f4dc7650fecf030189e59b72784cc06835a1b639
Bruno Roque <brunoroque06@gmail.com>
2025-09-17 16:46:10 +0200

    Use svg favicon

--- build.js
+++ build.js
@@ -1,18 +1,8 @@
 let fs = require("fs");
-let sharp = require("sharp");
 
 (async () => {
-  const fav = "favicon.png";
-
-  async function buildFavicons() {
-    let svg = sharp("logo.svg");
-    await svg.resize(192).png().toFile(fav);
-  }
-
-  await buildFavicons();
-
   let dist = "dist";
-  let files = ["index.html", fav, "bruno-roque-resume.pdf"];
+  let files = ["index.html", "favicon.svg", "bruno-roque-resume.pdf"];
 
   fs.rmSync(dist, { force: true, recursive: true });
   fs.mkdirSync(dist);
